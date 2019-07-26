af77ae8144a21b9ab41a5258c2dfa5636992f8ca
kburnssr <50921973+kburnssr@users.noreply.github.com>
2019-07-26 16:21:13 -0700

    I have added all of the HTML/CSS. I'm still working on the music, images, and names of the artists.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,56 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<style type="text/css">
-		body {
-			background-color: red;
+(function(){
+	var numwins = 0;
+	var songs = [{
+		"title": "Abbey Road",
+		"artist": "Beatles",
+		"song": "assets/music/holdmyhand.mp3",
+		"image": "assets/images/beatles.jpg"
+	}, {
+		"title": "Satisfaction",
+		"artist": "Rolling Stones",
+		"song": "assets/music/satisfaction.mp3",
+		"image": "assets/images/rollingstones.jpg"
+	}, {
+		"title": "s",
+		"artist": "Rolling Stones",
+		"song": "assets/music/satisfaction.mp3",
+		"image": "assets/images/janisjoplin.jpg"
+	}];
+	var guesses = [];
+	var numguessesallowed = 20;
+	var songplaying;
+	var nextsongtoplay;
+	//initialize game
+	//start by pressing any key
+	//display place holders for letters
+	//render the game state
+	function init(){
+		nextsongtoplay = songs[Math.floor(Math.random()*songs.length)];
+		render();
+	}
+	function render(){
+//populate the board
+//populate selections
+		$("#numberofguesses").text(numguessesallowed - guesses.length);
+		$("#lettersalreadyguessed").text(guesses.join(", "));
+		var board = [];
+		for(var i in nextsongtoplay.artist){
+			if(guesses.indexOf(nextsongtoplay.artist[i]) >= 0){
+				board.push(nextsongtoplay.artist[i]);
+			} else{
+				board.push("_");
+			}
 		}
-	</style>
-</head>
-<body>
+		$("#currentword").text(board.join(" "));
 
-<!-- write the word script and hit tab -->
-<script type="text/javascript" src="../images/WordGuess.jpg">
-	//if the line does not end with a curly bracket, you end it with a ;
-	var x = 2; 
-
-	alert(x+1);
-
-	console.log(x-1); //1
-
-</script>
-</body>
-</html>
+	}
+	$(document).on("keydown",function(e){
+		if(guesses.indexOf(e.key) >= 0){
+			return;
+		}
+//		console.log(e.key);
+		guesses.push(e.key);
+		render();
+	})
+	init();
+})();
